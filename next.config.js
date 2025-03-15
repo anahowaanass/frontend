ab476274be0cf539fd59d9a3784da06863e20d21
anahowaanass <anahowaanass@gmail.com>
2025-03-15 04:49:29 +0000

    Fixed

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config');
+import { i18n } from './next-i18next.config';
 
 const nextConfig = {
   // output: 'export',
   reactStrictMode: true,
   images: {
-    domains: ['127.0.0.1', 'localhost'],
+    domains: ['127.0.0.1', 'localhost', 'frontend-seven-iota-51.vercel.app'],
     remotePatterns: [
       {
         hostname: '**.cyber-scale.me',
+      },
+      {
+        hostname: '**.vercel.app', // Allow remote images from Vercel domain
       },
     ],
   },
   i18n,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
